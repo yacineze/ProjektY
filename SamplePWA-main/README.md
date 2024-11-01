72594b9c071357b9805bca9e114f14a2e9ac141c
yacineze <104499580+yacineze@users.noreply.github.com>
2024-11-01 12:22:23 +0100

    Add files via upload

---
+++ SamplePWA-main/README.md
@@ -0,0 +1,2 @@
+# SamplePWA
+Sample PWA for MAS2024/25
---
+++ SamplePWA-main/sw.js
@@ -0,0 +1,47 @@
+// sw.js
+const CACHE_NAME = 'workdays-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-128x128.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting(); // Zwinge den Service Worker sofort zu aktivieren
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); // Zwinge die neuen Clients den neuen Service Worker zu verwenden
+});
+
+
+version = 77;
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
